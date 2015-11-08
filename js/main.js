21366ab3d1bcadde9e9160a0b6cb20864a382f42
ashleyengelhardt <ashley.engelhardt@gmail.com>
2015-11-08 15:40:49 -0500

    reworked site and added jobs section

--- js/main.js
+++ js/main.js
@@ -16,3 +16,18 @@ function changeButton() {
     setTimeout(resetForm, 1500);
 }
 
+// sticky footer
+/*
+$(window).bind("load", function () {
+    var footer = $("footer");
+    var pos = footer.position();
+    var height = $(window).height();
+    height = height - pos.top;
+    height = height - footer.height();
+    if (height > 0) {
+        footer.css({
+            'margin-top': height + 'px'
+        });
+    }
+});
+*/
